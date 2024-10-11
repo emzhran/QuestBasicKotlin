06611596b03e853b9523841865ac61b8995e955d
emzhran <zachran750@gmail.com>
2024-10-11 08:19:48 +0700

    penjelasan untuk code LambdaFunctions

--- app/src/main/java/com/example/questbasickotlin/LambdaFunction.kt
+++ app/src/main/java/com/example/questbasickotlin/LambdaFunction.kt
@@ -1,5 +1,9 @@
 package com.example.questbasickotlin
 
+//Lambda Functions adalah fungsi yang tidak memiliki nama
+//Lambda Functions biasanya digunakan untuk membuat fungsi yang sederhana
+//Lambda Functions menggunakan tanda panah (->) untuk menyelesaikan parameter dan body fungsi
+
 fun uppercaseString(string : String): String{
     return string.uppercase()
 }
