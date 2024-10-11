d4a786300e6c91034d87b1bd4c7d74184475681a
emzhran <zachran750@gmail.com>
2024-10-11 08:06:11 +0700

    TipeData

---
+++ app/src/main/java/com/example/questbasickotlin/TIpeData.kt
@@ -0,0 +1,34 @@
+package com.example.questbasickotlin
+
+fun main(){
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //int
+    val intValue: Int = 30
+    println ("Int: $intValue")
+
+    //float
+    val floatValue: Float = 50.6f
+    println("float: $floatValue")
+
+    //double
+    val doubleValue: Double = 60.6
+    println("double: $doubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("char: $charValue")
+
+    //boolean
+    val booleanValue: Boolean = true
+    println("boolean: $booleanValue")
+
+    //string
+    var stringValue: String = "Hello, World!"
+    println("string: $stringValue")
+
+    stringValue = "Hello, Kotlin!"
+    println("string: $stringValue")
+}
